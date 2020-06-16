f53e3f032a6d820080b06392d26305562cdadefe
marios <msthoma@yahoo.com>
2020-06-16 16:59:11 +0300

    add permission granting logic

--- app/src/main/java/com/example/lesiondetector/MainActivity.kt
+++ app/src/main/java/com/example/lesiondetector/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.example.lesiondetector
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.afollestad.assent.Permission
+import com.afollestad.assent.isAllGranted
+import com.afollestad.assent.runWithPermissions
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        if (isAllGranted(Permission.CAMERA)) {
+            text_view.text = "Camera permission granted"
+        }
+
+        runWithPermissions(Permission.CAMERA) {
+            text_view.text = "Camera permission granted"
+        }
     }
 }
