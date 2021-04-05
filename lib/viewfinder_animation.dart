153681854d12579019e279aa2d3766806400f2ad
marios <msthoma@yahoo.com>
2021-04-05 16:12:52 +0300

    migrate code to null safety

--- lib/viewfinder_animation.dart
+++ lib/viewfinder_animation.dart
@@ -10,8 +10,8 @@ class ViewfinderAnimation extends StatefulWidget {
   final Color color;
 
   const ViewfinderAnimation({
-    Key key,
-    this.size,
+    Key? key,
+    required this.size,
     this.duration = const Duration(milliseconds: 1200),
     this.color = Colors.white,
   }) : super(key: key);
@@ -22,8 +22,8 @@ class ViewfinderAnimation extends StatefulWidget {
 
 class _ViewfinderAnimationState extends State<ViewfinderAnimation>
     with SingleTickerProviderStateMixin {
-  AnimationController _controller;
-  Animation<double> _animation;
+  late AnimationController _controller;
+  late Animation<double> _animation;
 
   @override
   void dispose() {
@@ -59,7 +59,7 @@ class _ViewfinderAnimationState extends State<ViewfinderAnimation>
 class ViewfinderPainter extends CustomPainter {
   final Color color;
 
-  ViewfinderPainter({this.color});
+  ViewfinderPainter({required this.color});
 
   @override
   void paint(Canvas canvas, Size size) {
